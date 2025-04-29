f4d0319d3c818f05eb85b25f3150e34263c71493
Watcharapol Frong <frongbook@hotmail.com>
2025-04-29 14:30:59 +0700

    Update

--- notebooks/plot.R
+++ notebooks/plot.R
@@ -1,17 +1,42 @@
 library(dplyr)
 library(readr)
 library(tidyverse)
+library(rpart)
+library(rpart.plot)
+library(scales)
 
 rfm_df <- read.csv("rfm_dashboard.csv")
+
+rfm_df %>%
+  group_by(Segment) %>%
+  summarise(
+    Avg_Recency = mean(Recency),
+    SD_Recency = sd(Recency),
+    Avg_Frequency = mean(Frequency),
+    SD_Frequency = sd(Frequency),
+    Avg_Monetary = mean(Monetary),
+    SD_Monetary = sd(Monetary)
+  )
+
+
+tree_model <- rpart(Segment ~ Recency + Frequency + Monetary, data = rfm_df, method = "class")
+tree_model$variable.importance
 
 set.seed(30)
 rfm_split <- rfm_df %>%
   sample_frac(size = 0.3)
 
-ggplot(rfm_split, aes(Recency, Monetary, color = Segment)) +
-  geom_point(size = 3, alpha = 0.7) +
-  theme_minimal() +
+avg_recency <- mean(rfm_split$Recency)
+avg_frequency <- mean(rfm_split$Frequency)
+
+ggplot(rfm_split, aes(Recency, Frequency, color = Segment, size = Monetary)) +
+  geom_point(alpha = 0.7) +
+  geom_vline(xintercept = avg_recency, linetype = "dashed", color = "grey50") +
+  geom_hline(yintercept = avg_frequency, linetype = "dashed", color = "grey50") +
+  scale_size(labels = label_number(scale_cut = cut_short_scale())) +
   labs(title = "Customer Segmentation Scatter Plot",
        x = "Recency (Days Since Last Purchase)",
-       y = "Monetary (Total Spend)",
-       caption = "source: Online Retail from UCI")
+       y = "Frequency (Number of Purchases)",
+       size = "Monetary (Total Spend)",
+       caption = "source: Online Retail from UCI") +
+  theme_minimal()
